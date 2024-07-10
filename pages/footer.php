8de0bd1cb94489659f64a7413e57fb57b519092f
sonvlss <88413580+sonvlss@users.noreply.github.com>
2024-07-11 01:10:55 +0700

    Add files via upload

---
+++ pages/footer.php
@@ -0,0 +1,84 @@
+<footer>
+       <div class="footer-top-area">
+              <div class="container">
+                     <div class="row">
+                            <div class="col-md-3 col-sm-4">
+                                   <div class="footer-contact">
+                                          <img src="#" alt="">
+                                          <p>
+                                          Trang web được xây dựng bởi Nhóm 8, dưới đây là thông tin liên lạc:
+                                          </p>
+                                          <ul class="address">
+
+                                                 <li>
+                                                        <span class="fa fa-phone"></span>
+                                                       0848101975
+                                                 </li>
+                                                 <li>
+                                                        <span class="fa fa-github"></span>
+                                                        github.com
+                                                 </li>
+                                                 <li>
+                                                        <span class="fa fa-envelope-o"></span>
+                                                        vungocson1863@gmail.com
+                                                 </li>
+                                          </ul>
+                                   </div>
+                            </div>
+                            <div class="col-md-3 hidden-sm">
+                                   <div class="footer-tweets">
+                                          <div class="footer-title">
+                                                 <h3>Mô tả</h3>
+                                          </div>
+                                          <div class="twitter-text" style="float:left;">
+                                                 <p>Đây là trang web bán quần áo thời trang dựa trên trang web
+                                                        https://dirtycoins.vn/ được
+                                                        tạo ra nhằm phục vụ cho môn học lập trình web
+                                                      </p>
+                                                
+                                          </div>
+                                   </div>
+                            </div>
+                            <div class="col-md-3 col-sm-4">
+                                   <div class="footer-support">
+                                          <div class="footer-title">
+                                                 <h3>Họ và tên thành viên nhóm</h3>
+                                          </div>
+                                          <div class="footer-menu">
+                                                 <ul>
+                                                 <li><a href="#">Phạm Tuấn Hùng</a></li>
+                                                 <li><a href="#">Nguyễn Thành An</a></li>
+                                                 <li><a href="#">Đào Tuấn Phát</a></li>
+                                                 <li><a href="#">Vũ Ngọc Sơn</a></li>
+                                                 <li><a href="#">Hoàng Anh Đức</a></li>
+                                                 </ul>
+                                          </div>
+                                   </div>
+                            </div>
+                            <div class="col-md-3 col-sm-4">
+                                   <div class="footer-info" style="width: 170px;">
+                                          <div class="footer-title">
+                                                 <h3>Mã số sinh viên</h3>
+                                          </div>
+                                          <div class="footer-menu">
+                                                 <ul>
+                                                 <li><a href="#">72DCTT20023</a></li>
+                                                  <li><a href="#">72DCTT20040</a></li>
+                                                 <li><a href="#">72DCTT20039</a></li>
+                                                 <li><a href="#">72DCTT20002</a></li>
+                                                 <li><a href="#">72DCTT20025</a></li>
+
+
+                                                        
+                                                 </ul>
+                                          </div>
+                                   </div>
+                            </div>
+                     </div>
+              </div>
+       </div>
+       <!-- footer top area end -->
+       <!-- footer area start -->
+
+       <a href="#" id="scrollUp"><i class="fa fa fa-arrow-up"></i></a>
+</footer>
